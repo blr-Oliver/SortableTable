b89b831c1e99b114e1dac14e11280068647728ce
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-05-26 17:43:17 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,35 +1,31 @@
-function Loader(url, lag){
-	this.url = url;
-	this.lag = lag;
+function Loader(url, lag) {
+  this.url = url;
+  this.lag = lag;
 }
 
 Loader.prototype = {
-	load: function(sort, page, size){
+  load : function(sort, page, size) {
     var settings = {
-      method: 'GET',
-      dataType: 'json',
-      traditional: true,
-      url: this.url,
-      data: $.extend({}, {
-        sort: sort,
-        page: page,
-        size: size
+      method : 'GET',
+      dataType : 'json',
+      traditional : true,
+      url : this.url,
+      data : $.extend({}, {
+        sort : sort,
+        page : page,
+        size : size
       })
     };
-    if(this.lag > 0){
+    if (this.lag > 0) {
       var deferred = new $.Deferred();
-      setTimeout(function(settings){
-        $.ajax(settings).done(function(data, textStatus, jqXHR){
-          deferred.resolve(data, textStatus, jqXHR);
-        }).fail(function(jqXHR, textStatus, errorThrown){
-          deferred.rejectWith(this, jqXHR, textStatus, errorThrown);
-        });
-      }, this.lag, settings);
+      $.ajax(settings).then(this._delayedCall(deferred.resolve.bind(deferred), this.lag),
+          this._delayedCall(deferred.reject.bind(deferred), this.lag),
+          this._delayedCall(deferred.notify.bind(deferred), this.lag));
       return deferred.promise();
-    }else{
-
+    } else
       return $.ajax(settings);
-
-    }
-	}
+  },
+  _delayedCall : function(callback, delay) {
+    return setTimeout.bind(null, callback, delay);
+  }
 }
